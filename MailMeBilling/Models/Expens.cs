66899ce6ac1fbd5f3bd3fab2db766b05dd544e3b
ajith111 <30756256+ajith111@users.noreply.github.com>
2020-07-22 07:15:47 +0530

    up

--- MailMeBilling/Models/Expens.cs
+++ MailMeBilling/Models/Expens.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Org.BouncyCastle.Utilities.IO.Pem;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -14,15 +15,32 @@ namespace MailMeBilling.Models
         public string reason { get; set; }
         [Display(Name ="Amount")]
         public decimal amount { get; set; }
-
-
-
-
-
-
-
-
-
+        [Display(Name = "Reg No")]
+        public string regno { get; set; }
+        [Display(Name = "Driver Name")]
+        public string drivername { get; set; }
+        [Display(Name = "Mobile")]
+        public string drmobile { get; set; }
+        [Display(Name = "Tpye of Vehicle")]
+        public string vtype { get; set; }
+        [Display(Name = "Total KG")]
+        public string totalkg { get; set; }
+        [Display(Name = "Fule Amount")]
+        public string fuleamount { get; set; }
+        [Display(Name = "Fule Station")]
+        public string fulestation { get; set; }
+        [Display(Name = "Fule Type")]
+        public string ftype { get; set; }
+        [Display(Name = "Employee Name")]
+        public string employeename { get; set; }
+        [Display(Name = "Reason of Give")]
+        public string resonemp { get; set; }
+        [Display(Name = "Address")]
+        public string address { get; set; }
+        [Display(Name = "Mobile Number")]
+        public string empmobile { get; set; }
+        [Display(Name = "Id proof")]
+        public byte[] idpro { get; set; }
 
         public string branch { get; set; }
         [Display(Name ="Entry Date")]
