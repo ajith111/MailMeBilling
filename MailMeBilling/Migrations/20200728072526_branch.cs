2cbde347a2dd9882598830de22c36db560bf1a9f
ajith111 <30756256+ajith111@users.noreply.github.com>
2020-07-28 13:00:52 +0530

    update

---
+++ MailMeBilling/Migrations/20200728072526_branch.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MailMeBilling.Migrations
+{
+    public partial class branch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Aboutshop",
+                table: "branch",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "branch",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Mobile",
+                table: "branch",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Shopname",
+                table: "branch",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Aboutshop",
+                table: "branch");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "branch");
+
+            migrationBuilder.DropColumn(
+                name: "Mobile",
+                table: "branch");
+
+            migrationBuilder.DropColumn(
+                name: "Shopname",
+                table: "branch");
+        }
+    }
+}
--- MailMeBilling/Models/Branch.cs
+++ MailMeBilling/Models/Branch.cs
@@ -10,7 +10,14 @@ namespace MailMeBilling.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Branchname { get; set; }
+        public string Shopname { get; set; }
+        public string Aboutshop { get; set; }
+      
         public string BranchAddress { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+
+        public string Branchname { get; set; }
+      
     }
 }
